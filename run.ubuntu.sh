95fcf3fb6d6320d883a09c9610c4b3b47804d9fe
Debashis Ganguly <m.hasanzadeh.mofrad@gmail.com>
2018-05-22 11:01:37 -0400

    Update run.ubuntu.sh

--- run.ubuntu.sh
+++ run.ubuntu.sh
@@ -2,4 +2,4 @@
 # Ubuntu run script
 
 export PATH=/usr/local/openmpi/bin:$PATH
-mpirun -np 2 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8
+mpirun -np 2 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8 1
