88f745d4c7629efd043c04276a2393e943924579
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-06-01 15:11:51 -0400

    Update src/apps/

---
+++ dev/src/apps/graph_analytics/pr.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "pr.h"
+
+int main(int argc, char ** argv) {
+	std::cout << "PageRank" << std::endl;
+	return(0);
+}
+
---
+++ dev/src/apps/graph_analytics/pr.h
@@ -0,0 +1,9 @@
+#ifndef PR_H
+#define PR_H
+
+#include <cassert>
+#include <cmath> 
+
+using vid_t = uint32_t;  // vertex id
+
+#endif
