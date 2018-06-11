253c8bbf96e2f227c5127c8e86b9aaff23bd3f17
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-06-11 13:24:55 -0400

    Update run.ubuntu.sh

--- run.ubuntu.sh
+++ run.ubuntu.sh
@@ -2,6 +2,7 @@
 # Ubuntu run script
 
 #export PATH=/usr/local/openmpi/bin:$PATH
-export PATH=/home/mohammad/mpich/mpich-install/bin:$PATH
+export PATH=/home/moh/mpich/bin:$PATH:$PATH
+#which mpirun
 #mpirun -np 4 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8 1
 mpirun -np 4 bin/graph_analytics/pr ../graph500/rmat/rmat20.bin 1048576 20
