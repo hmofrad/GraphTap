6212999231a44091794c6c7576b3e9fb4ad0e8ed
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-06-13 00:30:48 -0400

    Update run.ubuntu.sh

--- run.ubuntu.sh
+++ run.ubuntu.sh
@@ -5,4 +5,4 @@
 export PATH=/home/moh/mpich/bin:$PATH:$PATH
 #which mpirun
 #mpirun -np 4 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8 1
-mpirun -np 4 bin/graph_analytics/pr ../graph500/rmat/rmat20.bin 1048576 20
+mpirun -np 4 bin/graph_analytics/pr ~/graph500/rmat/rmat20.bin 1048576 20
