15316a1e756d85e3aa01a0017bb30a394077b14e
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-06-13 00:35:38 -0400

    Update dev/run.sh

--- dev/run.sh
+++ dev/run.sh
@@ -2,4 +2,6 @@
 # Ubuntu run script
 
 export PATH=/home/moh/mpich/bin:$PATH:$PATH
-mpirun -np 4 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8 1
+#mpirun -np 4 bin/graph_analytics/degree ~/graph500/rmat/rmat20.bin 1048576 1
+mpirun -np 4 bin/graph_analytics/degree data/graph_analytics/g1_8_8_13.bin 8 1
+#mpirun -np 4 bin/graph_analytics/pr data/graph_analytics/g1_8_8_13.bin 8 1
