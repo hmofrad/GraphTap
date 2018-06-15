da8cb27f170145ea44ac181ee4eb15e571edb678
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-06-15 17:53:08 -0400

    Update dev/src/apps/graph_analytics/pr.cpp

--- dev/src/apps/graph_analytics/pr.cpp
+++ dev/src/apps/graph_analytics/pr.cpp
@@ -4,6 +4,7 @@
 #include "utils/dist_timer.h"
 #include "pr.h"
 
+#include <iostream>
 
 /* Calculate Pagerank for a directed input graph. */
 
@@ -12,14 +13,11 @@ void run(std::string filepath, vid_t nvertices, uint32_t niters)
 {
   /* Calculate out-degrees */
   Graph<ew_t> GR; // reverse graph for out-degree
-  //std::cout << GR.A << std::endl; 
+  GR.load_directed(true, filepath, nvertices, true);  // reverse
+  //GR.load_directed(true, filepath, nvertices, true, false, Hashing::BUCKET, Partitioning::_1D_ROW);  // reverse
 
-  
-  //  GR.load_directed(true, filepath, nvertices, true);  // reverse
-  GR.load_directed(true, filepath, nvertices, true, false, Hashing::BUCKET, Partitioning::_2D);  // reverse
   DegVertex<ew_t> vp_degree(&GR, true);  // stationary
 
-  //exit(0);
   DistTimer degree_timer("Degree Execution");
   vp_degree.execute(1);
   degree_timer.stop();
@@ -29,8 +27,8 @@ void run(std::string filepath, vid_t nvertices, uint32_t niters)
 
   /* Calculate Pagerank */
   Graph<ew_t> G;
-  //G.load_directed(true, filepath, nvertices);
-  G.load_directed(true, filepath, nvertices, false, false, Hashing::BUCKET, Partitioning::_2D);
+  G.load_directed(true, filepath, nvertices);
+  //G.load_directed(true, filepath, nvertices, false, false, Hashing::BUCKET, Partitioning::_1D_ROW);
 
   /* Pagerank initialization using out-degrees */
   PrVertex vp(&G, true);  // stationary
@@ -64,6 +62,7 @@ void run(std::string filepath, vid_t nvertices, uint32_t niters)
 
 int main(int argc, char* argv[])
 {
+  //Env::init();
   Env::init(RankOrder::KEEP_ORIGINAL);
 
   /* Print usage. */
@@ -78,10 +77,95 @@ int main(int argc, char* argv[])
   std::string filepath = argv[1];
   vid_t nvertices = (vid_t) std::atol(argv[2]);
   uint32_t niters = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
-  std::cout << Env::rank << "," << Env::cpuname << "," << Env::cpuid << std::endl;
+  
+  //int root = -1;
+  //if(!Env::rank)
+   // root = Env::rank;
+  std::cout << Env::rank << "," << Env::cpu_name << "," << Env::cpu_id << std::endl;
+  std::vector<int> cpu_ids(Env::nranks);
+  
+  
+   
+  MPI_Gather(&Env::cpu_id, 1, MPI_INT, cpu_ids.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+  
+  //MPI_MAX_PROCESSOR_NAME
+  
+  if(!Env::rank)
+  {
+	  for(int i = 0; i < Env::nranks; i++) 
+		  std::cout << i << ":" << cpu_ids[i] << std::endl;
+  }
+  
+ 
+
+  int cpu_name_len = strlen(Env::cpu_name);
+  std::vector<int> recvcounts(Env::nranks);
+
+  MPI_Gather(&cpu_name_len, 1, MPI_INT, recvcounts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+  
+  
+  if(!Env::rank)
+  {
+	  for(int i = 0; i < Env::nranks; i++) 
+		  std::cout << i << ":" << recvcounts[i] << std::endl;
+  }
+
+  std::vector<int> displacement(Env::nranks);
+  int total_length = 0;
+  char *total_string = NULL;
+  if(!Env::rank)
+  {
+    displacement[0] = 0 ;
+    total_length += recvcounts[0] + 1;
+	for (int i=1; i < Env::nranks; i++) 
+	{
+	  total_length += recvcounts[i] + 1;
+	  displacement[i] = displacement[i-1] + recvcounts[i-1] + 1;
+	}
+    total_string = (char *) malloc(total_length * sizeof(char));
+	memset(total_string, 0, total_length);
+  }
+
+  //char *mystring = (char *)strings[Env::nranks];
+  //int mylen = strlen(mystring);
 
 
-  run(filepath, nvertices, niters);
+  //MPI_Gatherv(&Env::cpu_name, cpu_name_len, MPI_CHAR, total_string, recvcounts.data(), displacement.data(), MPI_CHAR, 0, MPI_COMM_WORLD);
+  //MPI_Gather(&Env::cpu_name, cpu_name_len, MPI_CHAR, total_string, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+   if (!Env::rank) {
+	   for(int i = 0; i < Env::nranks; i++) 
+	   {
+		   char *j = total_string + displacement[i];
+           printf("%d: <%s> %d %d %d\n", Env::rank, j, strlen(j), recvcounts[i], displacement[i]);
+	   }
+		   
+		
+        //free(totalstring);
+        //free(displs);
+        //free(recvcounts);
+    }
+
+ 
+  
+  //MPI_Allgather(&Env::cpu_id, 1, MPI_INT, cpu_ids.data(), 1, MPI_INT, MPI_COMM_WORLD);
+  
+  //MPI_Allgather(&Env::cpu_id, 1, MPI_INT, cpu_ids.data(), 1, MPI_INT, MPI_COMM_WORLD);
+  
+
+  /*
+  if(!Env::rank) 
+  {
+	  
+  }
+  else
+  {
+     MPI_Send(&Env::cpuid, 1, MPI_INT, Env::rank, 0, MPI_COMM_WORLD);	  
+  }
+*/	  
+  
+  
+  
+  //run(filepath, nvertices, niters);
 
   Env::finalize();
   return 0;
