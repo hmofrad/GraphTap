856db99e7503876b13af70540d29a574d47db339
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-08-14 14:07:16 -0400

    Update csr.hpp

--- dev/test/csr.hpp
+++ dev/test/csr.hpp
@@ -24,15 +24,11 @@ CSR<Weight, Integer_Type>::CSR(Integer_Type nnz_, Integer_Type nrows_plus_one_)
 {
     nnz = nnz_;
     nrows_plus_one = nrows_plus_one_;
-    A = new struct basic_storage<Weight, Integer_Type>(nnz_);
+    A = new struct basic_storage<Weight, Integer_Type>(nnz);
     
-    IA = new struct basic_storage<Integer_Type, Integer_Type>(nrows_plus_one_);
+    IA = new struct basic_storage<Integer_Type, Integer_Type>(nrows_plus_one);
     
-    JA = new struct basic_storage<Integer_Type, Integer_Type>(nnz_);
-    
-    //A_n = nnz;
-    //A_nbytes 
-        
+    JA = new struct basic_storage<Integer_Type, Integer_Type>(nnz);
 }
 
 template<typename Weight, typename Integer_Type>
@@ -41,4 +37,38 @@ CSR<Weight, Integer_Type>::~CSR()
     delete A;
     delete IA;
     delete JA;
+}
+
+
+template<typename Weight, typename Integer_Type>
+struct CSC
+{
+    CSC(Integer_Type nnz_, Integer_Type ncols_plus_one_);
+    ~CSC();
+    Integer_Type nnz;
+    Integer_Type ncols_plus_one;
+    
+    struct basic_storage<Weight, Integer_Type> *VAL;
+    struct basic_storage<Integer_Type, Integer_Type> *ROW_INDEX;
+    struct basic_storage<Integer_Type, Integer_Type> *COL_PTR;
+};
+
+template<typename Weight, typename Integer_Type>
+CSC<Weight, Integer_Type>::CSC(Integer_Type nnz_, Integer_Type ncols_plus_one_)
+{
+    nnz = nnz_;
+    ncols_plus_one = ncols_plus_one_;
+    VAL = new struct basic_storage<Weight, Integer_Type>(nnz);
+    
+    ROW_INDEX = new struct basic_storage<Integer_Type, Integer_Type>(nnz);
+    
+    COL_PTR = new struct basic_storage<Integer_Type, Integer_Type>(ncols_plus_one);
+}
+
+template<typename Weight, typename Integer_Type>
+CSC<Weight, Integer_Type>::~CSC()
+{
+    delete VAL;
+    delete ROW_INDEX;
+    delete COL_PTR;
 }
