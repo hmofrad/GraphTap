3e63340dfe4a24bd8131c5744d7015956871fbc3
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-08-24 13:32:59 -0400

    Update vector.hpp

--- dev/test/vector.hpp
+++ dev/test/vector.hpp
@@ -7,6 +7,9 @@
  * (e) m.hasanzadeh.mofrad@gmail.com 
  */
 
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
 template<typename Weight = char, typename Integer_Type = uint32_t, typename Fractional_Type = float>
 struct Segment
 {
@@ -22,11 +25,13 @@ struct Segment
             uint32_t leader_rank_rg_, uint32_t leader_rank_cg_, bool allocated_);
     ~Segment();
     void allocate();
+    void allocate(Integer_Type n);
     void del_seg();
     
     struct Basic_Storage<Fractional_Type, Integer_Type> *D;
 
     Integer_Type nrows, ncols;
+    uint32_t g;
     uint32_t rg, cg;
     uint32_t leader_rank;
     uint32_t leader_rank_rg;
@@ -63,6 +68,12 @@ void Segment<Weight, Integer_Type, Fractional_Type>::allocate()
 }
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
+void Segment<Weight, Integer_Type, Fractional_Type>::allocate(Integer_Type n)
+{
+    D = new struct Basic_Storage<Fractional_Type, Integer_Type>(n);
+}
+
+template<typename Weight, typename Integer_Type, typename Fractional_Type>
 void Segment<Weight, Integer_Type, Fractional_Type>::del_seg()
 {
     delete D;
@@ -74,18 +85,23 @@ class Vector
     template<typename Weight_, typename Integer_Type_, typename Fractional_Type_>
     friend class Graph;
     
+    //template<typename Weight__, typename Integer_Type__, typename Fractional_Type__>
+    //friend class Matrix;
+    
     public:
         Vector();
         Vector(Integer_Type nrows_, Integer_Type ncols_, uint32_t nrowgrps_, uint32_t ncolgrps_,
                Integer_Type tile_height_, Integer_Type tile_width_, uint32_t owned_segment_,
                std::vector<uint32_t> &leader_ranks, std::vector<uint32_t> &leader_ranks_rg,
                std::vector<uint32_t> &leader_ranks_cg, std::vector<int32_t> &local_segments_);
-       Vector(Integer_Type nrows_, Integer_Type ncols_, uint32_t nrowgrps_, uint32_t ncolgrps_,
+        Vector(Integer_Type nrows_, Integer_Type ncols_, uint32_t nrowgrps_, uint32_t ncolgrps_,
                Integer_Type tile_height_, Integer_Type tile_width_, uint32_t owned_segment_,
                std::vector<uint32_t> &leader_ranks, std::vector<uint32_t> &leader_ranks_rg,
                std::vector<uint32_t> &leader_ranks_cg);
+        Vector(Integer_Type nrows_, std::vector<int32_t> &local_segments_);
         ~Vector();
         void del_vec();
+        void del_vec_1();
         
         std::vector<struct Segment<Weight, Integer_Type, Fractional_Type>> segments;
         std::vector<int32_t> local_segments;
@@ -94,13 +110,33 @@ class Vector
         Integer_Type nrows, ncols;
         uint32_t nrowgrps, ncolgrps;
         Integer_Type tile_height, tile_width;
-
+        uint32_t vector_length;
         //void init_vec(std::vector<uint32_t> &diag_ranks, std::vector<int32_t>& local_segments);
         //void init_vec(std::vector<uint32_t> &diag_ranks);
 };
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
 Vector<Weight, Integer_Type, Fractional_Type>::Vector() {};
+
+
+template<typename Weight, typename Integer_Type, typename Fractional_Type>
+Vector<Weight, Integer_Type, Fractional_Type>::Vector(Integer_Type nrows_, std::vector<int32_t> &local_segments_)
+{
+    nrows = nrows_;
+    local_segments = local_segments_;
+    vector_length = local_segments.size();
+    // Reserve the 1D vector of segments. 
+    segments.resize(vector_length);
+    for(uint32_t i = 0; i < vector_length; i++)
+    {
+        segments[i].allocate(nrows);
+        segments[i].allocated = true;
+        segments[i].g = local_segments[i];
+        #ifdef PREFETCH
+        madvise(segments[i].D->data, segments[i].D->nbytes, MADV_SEQUENTIAL);
+        #endif
+    }
+}
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
 Vector<Weight, Integer_Type, Fractional_Type>::Vector(Integer_Type nrows_, Integer_Type ncols_,
@@ -150,7 +186,7 @@ Vector<Weight, Integer_Type, Fractional_Type>::Vector(Integer_Type nrows_, Integ
             segments[s].allocated = true;
         }
     }
-};
+}
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>    
 Vector<Weight, Integer_Type, Fractional_Type>::Vector(Integer_Type nrows_, Integer_Type ncols_,
@@ -192,6 +228,23 @@ template<typename Weight, typename Integer_Type, typename Fractional_Type>
 Vector<Weight, Integer_Type, Fractional_Type>::~Vector() {}
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
+void Vector<Weight, Integer_Type, Fractional_Type>::del_vec_1()
+{
+    //uint32_t i = 0;
+    //for(int32_t s: local_segments)
+    for(uint32_t i = 0; i < vector_length; i++)
+    //for(int32_t s: local_segments)
+    //{
+    {
+        if(segments[i].allocated)
+        {
+            segments[i].del_seg();
+        }
+       // i++;
+    }
+}
+
+template<typename Weight, typename Integer_Type, typename Fractional_Type>
 void Vector<Weight, Integer_Type, Fractional_Type>::del_vec()
 {
     for (uint32_t i = 0; i < ncolgrps; i++)
@@ -202,3 +255,4 @@ void Vector<Weight, Integer_Type, Fractional_Type>::del_vec()
         }
     }
 }
+#endif
