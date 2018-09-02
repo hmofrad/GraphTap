746a35d23673cc84ab31c2b287ae13a09591be85
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-09-02 14:03:59 -0400

    Update main.cpp

--- dev/test/main.cpp
+++ dev/test/main.cpp
@@ -58,7 +58,7 @@ struct Generic_functions
 
 int main(int argc, char **argv)
 {
-    bool comm_split = false;
+    bool comm_split = true;
     Env::init(comm_split);
     //printf("rank=%d,nranks=%d,is_master=%d\n", Env::rank, Env::nranks, Env::is_master);
     
