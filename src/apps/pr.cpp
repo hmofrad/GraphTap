651e1d734f2ca9dfaf84ccf05ed980683c3efa63
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-09-26 22:39:13 -0400

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -3,12 +3,13 @@
  * (c) Mohammad Mofrad, 2018
  * (e) m.hasanzadeh.mofrad@gmail.com 
  */
- 
-#include "env.hpp"
-#include "graph.hpp"
-#include "vertex_program.hpp"
+
 #include <iostream>
 #include <unistd.h>
+ 
+#include "mpi/env.hpp"
+#include "mat/graph.hpp"
+#include "vp/vertex_program.hpp"
 
 /* HAS_WEIGHT macro will be defined by compiler.
    So, you don't have to change this.   
