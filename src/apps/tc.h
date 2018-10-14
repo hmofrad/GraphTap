e694d38d289cbeea127cd16f1bd4e47449c696e1
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-13 22:55:08 -0400

    Update src/apps/tc.h

--- src/apps/tc.h
+++ src/apps/tc.h
@@ -8,23 +8,16 @@
 #define TC_H 
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
-class TC_state
+class TC_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type>
 {
     public: 
-        TC_state(){};
-        ~TC_state(){};    
-        bool initializer(Fractional_Type &v1, Fractional_Type &v2) 
+        using Vertex_Program<Weight, Integer_Type, Fractional_Type>::Vertex_Program;  // inherit constructors
+        virtual bool initializer(Fractional_Type &v1, Fractional_Type &v2) 
         {
             v1 = v2;
             return(true);
         }
-
-        void combiner(Fractional_Type &y1, Fractional_Type &y2) 
-        {
-            ;
-        }
-        
-        bool applicator(Fractional_Type &y, Fractional_Type &v) 
+        virtual bool applicator(Fractional_Type &y, Fractional_Type &v) 
         {
             return(true);
         }      
