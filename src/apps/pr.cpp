5b1b8274a75702178de3a418c0fa65ca88bdfe74
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-13 22:54:34 -0400

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
     bool parallel_edges = true;
     Tiling_type TT = _2D_;
     Compression_type CT = _CSC_;
-    Filtering_type FT = _SOME_;
+    Filtering_type FT = _NONE_;
     bool parread = true;
     
     /* Degree execution */
@@ -73,10 +73,7 @@ int main(int argc, char **argv)
     bool gather_depends_on_apply = false;
     Ordering_type OT = _ROW_;
     Degree_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, tc_family, OT);
-    
     V.execute(1);
-    //V.checksum();
-    //V.display();
     G.free();
     Env::barrier();
     
@@ -86,85 +83,14 @@ int main(int argc, char **argv)
     GR.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
     PageRank_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, tc_family, OT);
     VR.initialize(&V);
-    VR.execute(num_iterations);
-    //VR.checksum();
-    //VR.display(); 
-    
     V.free();
-    
-    
+    VR.execute(num_iterations);
     GR.free();
     VR.free();
     
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    Env::print_time("Degree end-to-end", elapsed_secs);
+    Env::print_time("PageRank end-to-end", elapsed_secs);
     Env::finalize();
     return(0);
-    
-    /*
-    // Degree execution 
-    Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
-    bool stationary = true;
-    bool tc_family = false;
-    bool gather_depends_on_apply = false;
-    Ordering_type OT = _ROW_;
-    // Register triangle counting function pointer handles
-    PR_state<wp, ip, fp> Pr_state;
-    auto initializer = std::bind(&PR_state<wp, ip, fp>::initializer, Pr_state, std::placeholders::_1, std::placeholders::_2);
-    auto messenger   = std::bind(&PR_state<wp, ip, fp>::messenger,   Pr_state, std::placeholders::_1, std::placeholders::_2);
-    auto combiner    = std::bind(&PR_state<wp, ip, fp>::combiner,    Pr_state, std::placeholders::_1, std::placeholders::_2);    
-    auto applicator  = std::bind(&PR_state<wp, ip, fp>::applicator,  Pr_state, std::placeholders::_1, std::placeholders::_2);
-    // Run vertex program
-    time1 = Env::clock();
-    
-    Vertex_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, tc_family, OT); 
-    V.init(initializer);
-    
-    V.scatter_gather(messenger);
-    V.combine(combiner);
-    V.apply(applicator);  
-    time2 = Env::clock();
-    Env::print_time("Degree execution", time2 - time1);
-    
-    V.checksum();
-    V.display();
-    G.free();
-    Env::barrier();
-    
-    // Vertex execution 
-    transpose = true;
-    Graph<wp, ip, fp> GR;    
-    GR.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
-    // Register triangle counting function pointer handles
-    auto messenger1  = std::bind(&PR_state<wp, ip, fp>::messenger1,   Pr_state, std::placeholders::_1, std::placeholders::_2);
-    auto applicator1  = std::bind(&PR_state<wp, ip, fp>::applicator1,  Pr_state, std::placeholders::_1, std::placeholders::_2);
-    time1 = Env::clock();
-    Vertex_Program<wp, ip, fp> VR(GR, stationary, OT);   
-    fp alpha = 0.15;
-    fp v = alpha;
-    VR.init(initializer, v, &V);
-    V.free();
-    uint32_t iter = 0;
-    uint32_t niters = num_iterations;
-    while(iter < niters)
-    {
-        iter++;
-        VR.scatter_gather(messenger1);
-        VR.combine(combiner);   
-        VR.apply(applicator1);
-        Env::print_me("Pagerank iteration: ", iter);
-    }
-    time2 = Env::clock();    
-    Env::print_time("Vertex execution", time2 - time1);
-
-    VR.checksum();
-    VR.display(); 
-    
-    VR.free();
-    GR.free();
-    */
-    Env::finalize();
-    return(0);   
 }
