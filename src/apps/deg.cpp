3271b6b7617a7a8fa42d2533f95f9af8f09bd5d9
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-26 16:31:53 -0400

    Update src/apps/deg.cpp

--- src/apps/deg.cpp
+++ src/apps/deg.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
             std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices>\""
                       << std::endl;
         }    
-        Env::exit(1);
+        std::exit(1);
     }
     std::string file_path = argv[1]; 
     ip num_vertices = std::atoi(argv[2]);
@@ -36,26 +36,20 @@ int main(int argc, char **argv)
     Compression_type CT = _CSC_;
     Filtering_type FT = _SOME_;
     bool parread = true;
-    Graph<wp, ip, fp> G;    
+    Graph<wp, ip, ip> G;    
     G.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
     bool stationary = true;
     bool tc_family = false;
     bool gather_depends_on_apply = false;
-    bool gather_depends_on_iter  = false;
+    bool apply_depends_on_iter  = false;
     Ordering_type OT = _ROW_;
-    
-    //Degree_State<wp, ip, fp> S;
-    //Vertex_State<wp, ip, fp> &SB = static_cast<Degree_State<wp, ip, fp>&>(S);
-    //Vertex_State<wp, ip, fp> *SB = &S;
-    //printf("sizeee=%lu, %lu\n", sizeof(S), sizeof(SB));
-    
-    Degree_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, gather_depends_on_iter, tc_family, OT);
-    V.execute(num_iterations); // Degree execution
+    /* Degree execution */
+    Degree_Program<wp, ip, ip> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    V.execute(num_iterations);
     V.checksum();
     V.display();
     V.free();
     G.free();
-    
     
     double time2 = Env::clock();
     Env::print_time("Degree end-to-end", time2 - time1);
