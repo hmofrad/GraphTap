08d9d3843c373d122d8e8a64fc079612a4ad9bc6
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-27 02:40:01 -0400

    Update src/apps/pr.h

--- src/apps/pr.h
+++ src/apps/pr.h
@@ -12,31 +12,26 @@
 fp tol = 1e-5;
 fp alpha = 0.15;
 
-
-struct PageRank_State : Degree_State
+struct PR_State : Deg_State
 {
     fp rank = alpha;
-    ip get_state(){return(rank);};
+    fp get_state(){return(rank);};
     std::string print_state(){return("Rank=" + std::to_string(rank) + ",Degree=" + std::to_string(degree));};
 };
 
 template<typename Weight, typename Integer_Type, typename Fractional_Type>
-class PageRank_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type, PageRank_State>
-//class PageRank_Program : public Vertex_Program<wp, ip, fp, PageRank_State>
+class PR_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type, PR_State>
 {
     public: 
-        using Vertex_Program<Weight, Integer_Type, Fractional_Type, PageRank_State>::Vertex_Program;  // inherit constructors
-        //using Vertex_Program<wp, ip, fp, PageRank_State>::Vertex_Program;  // inherit constructors
-        virtual bool initializer(Integer_Type vid, PageRank_State &state, const State &other)
-        //virtual bool initializer(Fractional_Type &v1, Fractional_Type &v2) 
+        using Vertex_Program<Weight, Integer_Type, Fractional_Type, PR_State>::Vertex_Program;
+        virtual bool initializer(Integer_Type vid, PR_State &state, const State &other)
         {
-            state.degree = ((const Degree_State&) other).degree;
+            state.degree = ((const Deg_State&) other).degree;
             state.rank = alpha; //  Not necessary
-            //printf("%d %d %f\n", vid, state.degree, state.rank);
             return(true);
         }
 
-        virtual Fractional_Type messenger(PageRank_State &state) 
+        virtual Fractional_Type messenger(PR_State &state) 
         {
             return( (state.degree) ? (state.rank / state.degree) : 0 );
         }
@@ -46,8 +41,7 @@ class PageRank_Program : public Vertex_Program<Weight, Integer_Type, Fractional_
             y1 += y2;
         }
         
-        //virtual bool applicator(Fractional_Type &v, Fractional_Type &y) 
-        virtual bool applicator(PageRank_State &state, const Fractional_Type &y) 
+        virtual bool applicator(PR_State &state, const Fractional_Type &y) 
         {
             Fractional_Type tmp = state.rank;
             state.rank = alpha + (1.0 - alpha) * y;
