0844a2461fd0b5d86a5fe6e5329bc6dc53dbff24
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-27 02:39:10 -0400

    Update src/apps/cc.cpp

--- src/apps/cc.cpp
+++ src/apps/cc.cpp
@@ -9,32 +9,8 @@
  
 #include "mpi/env.hpp"
 #include "mat/graph.hpp"
-#include "vp/vertex_program.hpp"
 
 #include "cc.h"
-
-/* HAS_WEIGHT macro will be defined by compiler.
-   So, you don't have to change this.   
-   make WEIGHT="-DHASWEIGHT"         */
-   
-using em = Empty; // Weight (default is Empty)
-#ifdef HAS_WEIGHT
-using wp = uint32_t;
-#else
-using wp = em;
-#endif
-
-/*  Integer precision (default is uint32_t)
-    Controls the number of vertices,
-    the engine can possibly process
-*/
-using ip = uint32_t;
-
-/*
-    Fractional precision (default is float)
-    Controls the precision of values.
-*/
-using fp = uint32_t;
 
 int main(int argc, char **argv)
 { 
@@ -42,9 +18,9 @@ int main(int argc, char **argv)
     Env::init(comm_split);    
     double time1 = Env::clock();   
     
-    if(argc != 4)  {
+    if(argc != 3)  {
         if(Env::is_master) {
-            std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices> [<num_iterations=0>]\""
+            std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices>\""
                       << std::endl;
         }    
         Env::exit(1);
@@ -52,20 +28,21 @@ int main(int argc, char **argv)
     
     std::string file_path = argv[1]; 
     ip num_vertices = std::atoi(argv[2]);
-    uint32_t num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
+    uint32_t num_iterations = 0;
     num_iterations = 0;
     bool directed = false;
     bool transpose = false;
+    bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = false;
     Tiling_type TT = _2D_;
-    Compression_type CT = _CSC_;
+    Compression_type CT = _CSR_;
     Filtering_type FT = _SOME_;
     bool parread = true;
     
     /* Connected component execution */
     Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, parread);
     bool stationary = false;
     bool tc_family = false;
     bool gather_depends_on_apply = true;
@@ -77,6 +54,7 @@ int main(int argc, char **argv)
     V.display();
     V.free();
     G.free();
+
     double time2 = Env::clock();
     Env::print_time("Connected component end-to-end", time2 - time1);
     Env::finalize();
