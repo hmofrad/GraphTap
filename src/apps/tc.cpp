b61ad71404fdc12ce29b0be71356033f18a1a3cd
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-27 02:40:45 -0400

    Update src/apps/tc.cpp

--- src/apps/tc.cpp
+++ src/apps/tc.cpp
@@ -9,32 +9,8 @@
  
 #include "mpi/env.hpp"
 #include "mat/graph.hpp"
-#include "vp/vertex_program.hpp"
 
 #include "tc.h" 
-
-/* HAS_WEIGHT macro will be defined by compiler.
-   So, you don't have to change this.   
-   make WEIGHT="-DHASWEIGHT"         */
-   
-using em = Empty; // Weight (default is Empty)
-#ifdef HAS_WEIGHT
-using wp = uint32_t;
-#else
-using wp = em;
-#endif
-
-/*  Integer precision (default is uint32_t)
-    Controls the number of vertices,
-    the engine can possibly process
-*/
-using ip = uint32_t;
-
-/*
-    Fractional precision (default is float)
-    Controls the precision of values.
-*/
-using fp = uint64_t; 
 
 int main(int argc, char **argv)
 {
@@ -42,19 +18,20 @@ int main(int argc, char **argv)
     Env::init(comm_split);  
     double time1 = Env::clock();    
     
-    if(argc != 4)  {
+    if(argc != 3)  {
         if(Env::is_master) {
-            std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices> [<num_iterations>]\""
+            std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices>\""
                       << std::endl;
         }    
-        Env::exit(1);
+        exit(1);
     }
     
     std::string file_path = argv[1]; 
     ip num_vertices = std::atoi(argv[2]);
-    uint32_t num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
+    ip num_iterations = 1;
     bool directed = true;
     bool transpose = true;
+    bool self_loops = false;
     bool acyclic = true;
     bool parallel_edges = false;
     Tiling_type TT = _2D_;
@@ -64,7 +41,7 @@ int main(int argc, char **argv)
     
     /* Triangle counting execution */
     Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, parread);
     bool stationary = false;
     bool tc_family = true;
     bool gather_depends_on_apply = false;
@@ -72,21 +49,22 @@ int main(int argc, char **argv)
     Ordering_type OT = _ROW_;
     // Run 1st vertex program and calculate ingoing adjacency list
     TC_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
-    V.execute(1);
+    V.execute(num_iterations);
     G.free();
     Env::barrier();
     
     transpose = false;
     Graph<wp, ip, fp> GR;    
-    GR.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
+    GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, parread);
     // Run 2nd vertex program and calculate outgoing adjacency list
     TC_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);  
-    VR.initialize(&V);
+    VR.initialize(V);
     V.free();
     VR.execute(1);
     VR.free();
     GR.free();
-
+    
+    
     double time2 = Env::clock();    
     Env::print_time("Triangle counting end-to-end", time2 - time1);
     Env::finalize();
