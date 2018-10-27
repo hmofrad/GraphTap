35cc010250c20a76912cb39cfc1c94d0df2220d3
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-10-27 02:41:15 -0400

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -18,12 +18,12 @@ int main(int argc, char **argv)
     Env::init(comm_split);    
     double time1 = Env::clock();
     
-    if(argc != 4)  {
+    if((argc != 3) and (argc != 4))  {
         if(Env::is_master) {
             std::cout << "\"Usage: " << argv[0] << " <file_path> <num_vertices> [<num_iterations=INF>]\""
                       << std::endl;
         }    
-        Env::exit(1);
+        exit(1);
     }
     
     std::string file_path = argv[1]; 
@@ -31,22 +31,24 @@ int main(int argc, char **argv)
     ip num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
     bool directed = true;
     bool transpose = false;
+    bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = true;
     Tiling_type TT = _2D_;
     Compression_type CT = _CSC_;
-    Filtering_type FT = _NONE_;
+    Filtering_type FT = _SOME_;
     bool parread = true;
     
     /* Degree execution */
     Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, parread);
     bool stationary = true;
     bool tc_family = false;
     bool gather_depends_on_apply = false;
     bool apply_depends_on_iter  = false;
     Ordering_type OT = _ROW_;
-    Degree_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    Deg_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    
     V.execute(1);
     V.checksum();
     G.free();
@@ -54,9 +56,8 @@ int main(int argc, char **argv)
     
     transpose = true;
     Graph<wp, ip, fp> GR;    
-    GR.load(file_path, num_vertices, num_vertices, directed, transpose, acyclic, parallel_edges, TT, CT, FT, parread);
-    PageRank_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
-    //Vertex_Program<wp, ip, fp, Degree_State> *VP = &V;
+    GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, parread);
+    PR_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
     VR.initialize(V);
     V.free();
     VR.execute(num_iterations); // Vertex execution
@@ -64,7 +65,7 @@ int main(int argc, char **argv)
     VR.display();
     GR.free();
     VR.free();
-
+    
     double time2 = Env::clock();
     Env::print_time("PageRank end-to-end", time2 - time1);
     Env::finalize();
