0e65c099a03cbb6f78a7c98d74063956a98d3756
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-11-04 17:07:03 -0500

    Update src/apps/sssp.h

--- src/apps/sssp.h
+++ src/apps/sssp.h
@@ -31,10 +31,7 @@ using fp = uint32_t;
 struct SSSP_State
 {
     ip distance = INF;
-    //ip hops = INF;
-    //ip vid = 0;
     ip get_state(){return(distance);};
-    //ip get_inf(){return(INF);};    
     std::string print_state(){return("Distance=" + std::to_string(distance));};
 };
 
@@ -48,51 +45,32 @@ class SSSP_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type
         {
             if(vid == root)
             {
-                //state.vid = vid;
                 state.distance = 0;
-                //printf("%d\n", vid);
                 return(true);
             }
             else
             {
-                //state.vid = vid;
                 state.distance = INF; // Not necessary
                 return(false);
             }
-            
-            
         }
         
         virtual Fractional_Type messenger(SSSP_State &state) 
-        //virtual Fractional_Type messenger(Fractional_Type &v, Fractional_Type &s) 
         {
             return(state.distance);
         }
 
-        //#ifdef HAS_WEIGHT
         virtual void combiner(Fractional_Type &y1, const Fractional_Type &y2, const Fractional_Type &w) 
         {
             Fractional_Type tmp = y2 + w;
             y1 = (y1 < tmp) ? y1 : tmp;
         }
-        //#else
             
         virtual void combiner(Fractional_Type &y1, const Fractional_Type &y2) 
         {
-            /*
-            if(y2 != INF)
-            {
-                if(y2 < y1)     
-                    y1 = y2;
-            }
-            */
             y1 = (y1 < y2) ? y1 : y2;
-            //printf("%d %d \n", y1 ,y2);
         }
-        
-        //#endif
-        
-        //virtual bool applicator(Fractional_Type &v, Fractional_Type &y) 
+
         virtual bool applicator(SSSP_State &state, const Fractional_Type &y)
         {
             Fractional_Type tmp = state.distance;
@@ -101,66 +79,10 @@ class SSSP_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type
             #else
             state.distance = (y < state.distance) ? y + 1 : state.distance;
             #endif
-            //state.distance = (y < state.distance) ? y : state.distance;
             return(tmp != state.distance);
             
-            //#ifdef HAS_WEIGHT
-            /*
-            Fractional_Type t = v;
-            v = (y < v) ? y : v;
-            return(t != v); 
-            */
-            
-            //#else
-              /*
-            if(state.distance != INF)
-                return(false); // already visited
-            else
-            {
-                if(y != INF)
-                {
-                    
-                    #ifdef HAS_WEIGHT
-                    state.distance = (y < state.distance) ? y : state.distance;
-                    #else
-                    state.distance = (y < state.distance) ? y + 1 : state.distance;
-                    //v = (y < v) ? y + 1: v;    
-                    //v = y + 1;
-                    #endif    
-                    
-                    return(true);
-                }
-                else
-                    return(false);
-            }
-            */
-            /*
-            
-            if(v != INF)
-                return(false); // already visited
-            else
-            {
-                if(y != INF)
-                {
-                    #ifdef HAS_WEIGHT
-                    v = (y < v) ? y : v;
-                    #else
-                    v = (y < v) ? y + 1: v;    
-                    //v = y + 1;
-                    #endif    
-                    return(true);
-                }
-                else
-                    return(false);
-            }
-            
-                A tmp = s.distance;
-    s.distance = std::min(s.distance, y);
-    return tmp != s.distance;
-            */
-            //#endif
-            
         }   
+        
         virtual Fractional_Type infinity()
         {
             return(INF);
