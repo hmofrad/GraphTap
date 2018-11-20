69e387be94b628ca69c2137fbad8feed6e7fe737
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-11-20 17:57:48 -0500

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -29,8 +29,8 @@ int main(int argc, char **argv)
     ip num_vertices = std::atoi(argv[2]);
     ip num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
     bool directed = true;
-    bool transpose = false;
-    //bool transpose = true;
+    //bool transpose = false;
+    bool transpose = true;
     bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = true;
@@ -48,28 +48,28 @@ int main(int argc, char **argv)
     bool tc_family = false;
     bool gather_depends_on_apply = false;
     bool apply_depends_on_iter  = false;
-    //Ordering_type OT = _COL_;
-    Ordering_type OT = _ROW_;
+    Ordering_type OT = _COL_;
+    //Ordering_type OT = _ROW_;
     Deg_Program<wp, ip, fp> V(G, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
     V.execute(1);
     V.checksum();
-    G.free();
+    //G.free();
     Env::barrier();
     
     OT = _ROW_;
-    transpose = true;
-    Graph<wp, ip, fp> GR;    
-    GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, HT, parread);
-    PR_Program<wp, ip, fp> VR(GR, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
-    //PR_Program<wp, ip, fp> VR(G, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    //transpose = true;
+    //Graph<wp, ip, fp> GR;    
+    //GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, HT, parread);
+    //PR_Program<wp, ip, fp> VR(GR, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    PR_Program<wp, ip, fp> VR(G, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
     VR.initialize(V);
     V.free();
     VR.execute(num_iterations); // Vertex execution
     VR.checksum();
     VR.display();
     VR.free();
-    GR.free();
-    //G.free();
+    //GR.free();
+    G.free();
     
     double time2 = Env::clock();
     Env::print_time("PageRank end-to-end", time2 - time1);
