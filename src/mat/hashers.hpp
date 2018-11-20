aafd8f6e5b3cac18cf7bc2277ab7d748ef878817
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-11-20 14:37:21 -0500

    Update src/mat/hashers.hpp

--- src/mat/hashers.hpp
+++ src/mat/hashers.hpp
@@ -18,49 +18,51 @@ enum Hashing_type
 
 class ReversibleHasher
 {
-public:
-  virtual ~ReversibleHasher() {}
-  virtual long hash(long v) const = 0;
-  virtual long unhash(long v) const = 0;
+    public:
+        virtual ~ReversibleHasher() {}
+        virtual long hash(long v) const = 0;
+        virtual long unhash(long v) const = 0;
 };
 
 class NullHasher : public ReversibleHasher
 {
-public:
-  NullHasher() {}
-  long hash(long v) const { return v; }
-  long unhash(long v) const { return v; }
+    public:
+        NullHasher() {}
+        long hash(long v) const { return v; }
+        long unhash(long v) const { return v; }
 };
 
 class SimpleBucketHasher : public ReversibleHasher
 {
-private:
-  const long multiplier = 128u; // For fine-granular load balance
-  long nparts = 0;
-  long height = 0;
-  long max_range = 0;
+    private:
+        const long multiplier = 128u; // For fine-granular load balance
+        long nparts = 0;
+        long height = 0;
+        long max_range = 0;
 
-public:
-  SimpleBucketHasher(long max_domain, long nbuckets)
-  {
-    nparts = nbuckets * multiplier;
-    height = max_domain / nparts;
-    max_range = height * nparts;
-  }
+    public:
+        SimpleBucketHasher(long max_domain, long nbuckets)
+        {
+            nparts = nbuckets * multiplier;
+            height = max_domain / nparts;
+            max_range = height * nparts;
+        }
 
-  long hash(long v) const {
-    if(v >= max_range) return v;
-    long col = (uint32_t) v % nparts;
-    long row = v / nparts;
-    return row + col * height;
-  }
+        long hash(long v) const 
+        {
+            if(v >= max_range) return v;
+            long col = (uint32_t) v % nparts;
+            long row = v / nparts;
+            return row + col * height;
+        }
 
-  long unhash(long v) const {
-    if(v >= max_range) return v;
-    long col = v / height;
-    long row = v % height;
-    return col + row * nparts;
-  }
+        long unhash(long v) const
+        {
+            if(v >= max_range) return v;
+            long col = v / height;
+            long row = v % height;
+            return col + row * nparts;
+        }
 };
 
 
