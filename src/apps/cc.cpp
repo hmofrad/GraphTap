4968d9bd2ac420a4bdd8df326e0e53d62cb541d9
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-11-20 14:42:13 -0500

    Update src/apps/cc.cpp

--- src/apps/cc.cpp
+++ src/apps/cc.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
     // Only CSC is supported for nonstationary algorithms
     Compression_type CT = _CSC_;
     Filtering_type FT = _SOME_;
-    Hashing_type HT = BUCKET;
+    Hashing_type HT = NONE;
     bool parread = true;
     
     /* Connected component execution */
