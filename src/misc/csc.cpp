3f7b354f2cabd68dd9541db9556deefcf63e39ad
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-11-27 01:07:41 -0500

    Update csc.cpp

---
+++ src/misc/csc.cpp
@@ -0,0 +1,122 @@
+/*
+ * csc.cpp: CSC SpMV implementation
+ * (c) Mohammad Mofrad, 2018
+ * (e) m.hasanzadeh.mofrad@gmail.com 
+ * Standalone compile commnad:
+ * g++ -o spmv spmv.cpp -std=c++14
+ */
+ 
+#include <iostream>
+#include <cstdlib> 
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cassert>
+#include <sys/mman.h>
+#include <cstring> 
+#include <vector>
+#include <algorithm>
+
+void init_csc(uint32_t nnz_, uint32_t ncols)
+{
+    nnz = nnz_;
+    ncols_plus_one = ncols + 1;
+    if((A = (uint32_t *) mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
+                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
+    {    
+        fprintf(stderr, "Error mapping memory\n");
+        exit(1);
+    }
+    memset(A, 0, nnz * sizeof(uint32_t));
+    
+    if((IA = (uint32_t *) mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
+                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
+    {    
+        fprintf(stderr, "Error mapping memory\n");
+        exit(1);
+    }
+    memset(IA, 0, nnz * sizeof(uint32_t));
+
+    if((JA = (uint32_t *) mmap(nullptr, ncols_plus_one * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
+                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
+    {    
+        fprintf(stderr, "Error mapping memory\n");
+        exit(1);
+    }
+    size =  nnz * (sizeof(uint32_t) + sizeof(uint32_t)) + (ncols_plus_one * sizeof(uint32_t));
+}
+
+void popu_csc()
+{
+    uint32_t i = 0;
+    uint32_t j = 1;
+    JA[0] = 0;
+    for(auto &triple: *triples)
+    {
+        while((j - 1) != triple.col)
+        {
+            j++;
+            JA[j] = JA[j - 1];
+        }                  
+        A[i] = 1;
+        JA[j]++;
+        IA[i] = triple.row;
+        i++;
+    }
+    while((j + 1) < (num_vertices + 1))
+    {
+        j++;
+        JA[j] = JA[j - 1];
+    }
+}
+
+void run_csc()
+{
+    init_csc(triples->size(), num_vertices);
+    popu_csc();
+    printf("[x]Compression is done\n");
+}
+
+void walk_csc()
+{
+    for(uint32_t j = 0; j < ncols_plus_one - 1; j++)
+    {
+        printf("j=%d\n", j);
+        for(uint32_t i = JA[j]; i < JA[j + 1]; i++)
+        {
+            printf("   i=%d j=%d\n", IA[i], j);
+        }
+    }
+}
+
+void init_csc()
+{
+    values.resize(num_vertices);
+    y.resize(num_vertices);
+    x.resize(num_vertices, 1);
+}
+
+void spmv_csc()
+{
+    for(uint32_t j = 0; j < ncols_plus_one - 1; j++)
+    {
+        for(uint32_t i = JA[j]; i < JA[j + 1]; i++)
+        {
+            y[IA[i]] += A[i] * x[j]; 
+            nOps++;
+            
+        }
+    }
+}
+
+void done_csc()
+{
+    
+    for(uint32_t i = 0; i < num_vertices; i++)
+            values[i] = y[i];
+    
+    for(uint32_t i = 0; i < num_vertices; i++)
+        value += values[i];
+    
+    
+}
