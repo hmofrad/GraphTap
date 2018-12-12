de323414dec29650566e67bac2666bf3abec2640
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-12 18:06:49 -0500

    Update dcsc_d.cpp

--- src/sim/dcsc_d.cpp
+++ src/sim/dcsc_d.cpp
@@ -1,5 +1,5 @@
 /*
- * dcsc_d.cpp: DCSC SpMSpV implementation
+ * dcsc_d_d.cpp: dcsc_d SpMSpV implementation
  * (c) Mohammad Mofrad, 2018
  * (e) m.hasanzadeh.mofrad@gmail.com 
  * Standalone compile commnad:
@@ -18,60 +18,60 @@
 #include <algorithm>
 
 
-uint32_t nnz_rows_;
-std::vector<char> rows_;
-std::vector<uint32_t> rows_val_;
-std::vector<uint32_t> rows2vals_;
-uint32_t nnz_cols_;
-std::vector<char> cols_;
-std::vector<uint32_t> cols_val_;
-std::vector<int> rows2cols_;
-uint32_t nnz_rows2cols_;
-std::vector<int> cols2rows_;
+uint32_t nnz_rows___;
+std::vector<char> rows___;
+std::vector<uint32_t> rows_val___;
+std::vector<uint32_t> rows2vals___;
+uint32_t nnz_cols___;
+std::vector<char> cols___;
+std::vector<uint32_t> cols_val___;
+std::vector<int> rows2cols___;
+uint32_t nnz_rows2cols___;
+std::vector<int> cols2rows___;
 
 
-void filtering_dcsc(uint32_t num_vertices)
+void filtering_dcsc_d(uint32_t num_vertices)
 {
-    rows_.resize(num_vertices);
-    rows_val_.resize(num_vertices);
-    cols_.resize(num_vertices);
-    cols_val_.resize(num_vertices);
+    rows___.resize(num_vertices);
+    rows_val___.resize(num_vertices);
+    cols___.resize(num_vertices);
+    cols_val___.resize(num_vertices);
     
     for(auto &triple: *triples)
     {
-        rows_[triple.row] = 1;
-        cols_[triple.col] = 1;
+        rows___[triple.row] = 1;
+        cols___[triple.col] = 1;
     }
     
     uint32_t i = 0, j = 0;
     for(uint32_t k = 0; k < num_vertices; k++)
     {
-        if(rows_[k] and cols_[k])
+        if(rows___[k] and cols___[k])
         {
-            rows2cols_.push_back(i);
-            cols2rows_.push_back(j);
+            rows2cols___.push_back(i);
+            cols2rows___.push_back(j);
         }
         
-        if(rows_[k])
+        if(rows___[k])
         {
-            rows2vals_.push_back(k);
-            rows_val_[k] = i;
+            rows2vals___.push_back(k);
+            rows_val___[k] = i;
             i++;
         }
-        if(cols_[k])
+        if(cols___[k])
         {
-            cols_val_[k] = j;
+            cols_val___[k] = j;
             j++;
         }
         //printf("%d %d %d\n", k, rows[k], cols[k]);
     }
-    nnz_rows_ = i;
-    nnz_cols_ = j;
-    nnz_rows2cols_ = rows2cols_.size();
+    nnz_rows___ = i;
+    nnz_cols___ = j;
+    nnz_rows2cols___ = rows2cols___.size();
     printf("[x]Filtering is done\n");
 }
 
-void init_dcsc(uint32_t nnz_, uint32_t ncols)
+void init_dcsc_d(uint32_t nnz_, uint32_t ncols)
 {
     nnz = nnz_;
     ncols_plus_one = ncols + 1;
@@ -97,37 +97,37 @@ void init_dcsc(uint32_t nnz_, uint32_t ncols)
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
-    size =  nnz * (sizeof(uint32_t) + sizeof(uint32_t)) + (ncols_plus_one * sizeof(uint32_t)) + (nnz_cols_ * sizeof(uint32_t)) + (nnz_rows_ * sizeof(uint32_t));
+    size =  nnz * (sizeof(uint32_t) + sizeof(uint32_t)) + (ncols_plus_one * sizeof(uint32_t)) + (nnz_cols___ * sizeof(uint32_t)) + (nnz_rows___ * sizeof(uint32_t));
     
 }
 
-void popu_dcsc()
+void popu_dcsc_d()
 {
     uint32_t i = 0;
     uint32_t j = 1;
     JA[0] = 0;
     for(auto &triple: *triples)
     {
-        while((j - 1) != cols_val_[triple.col])
+        while((j - 1) != cols_val___[triple.col])
         {
             j++;
             JA[j] = JA[j - 1];
         }                  
         A[i] = 1;
         JA[j]++;
-        IA[i] = rows_val_[triple.row];
+        IA[i] = rows_val___[triple.row];
         i++;
     }
 }
 
-void run_dcsc()
+void run_dcsc_d()
 {
-    init_dcsc(triples->size(), nnz_cols_);
-    popu_dcsc();
+    init_dcsc_d(triples->size(), nnz_cols___);
+    popu_dcsc_d();
     printf("[x]Compression is done\n");
 }
 
-void walk_dcsc()
+void walk_dcsc_d()
 {
     for(uint32_t j = 0; j < ncols_plus_one - 1; j++)
     {
@@ -139,36 +139,37 @@ void walk_dcsc()
     }
 }
 
-void init_dcsc_vecs()
+void init_dcsc_d_vecs()
 {
     values.resize(num_vertices);
-    y.resize(nnz_rows_);
-    x.resize(nnz_cols_, 1);
+    y.resize(nnz_rows___);
+    x.resize(nnz_cols___, 1);
 }
 
-void spmv_dcsc()
+void spmv_dcsc_d()
 {
     for(uint32_t j = 0; j < ncols_plus_one - 1; j++)
     {
         for(uint32_t i = JA[j]; i < JA[j + 1]; i++)
         {
-            y[rows_val_[IA[i]]] += A[i] * x[j]; 
+            y[rows_val___[IA[i]]] += A[i] * x[j]; 
             nOps++;
             
         }
     }
-    for(uint32_t i = 0; i < nnz_rows2cols_; i++)
+    
+    for(uint32_t i = 0; i < nnz_rows2cols___; i++)
     {
-        x[cols2rows_[i]] = y[rows2cols_[i]];
-        x[cols2rows_[i]] = 1;
+        x[cols2rows___[i]] = y[rows2cols___[i]];
+        x[cols2rows___[i]] = 1;
     }
 }
 
-void done_dcsc()
+void done_dcsc_d()
 {
     
-    for(uint32_t i = 0; i < nnz_rows_; i++)
-            values[rows2vals__[i]] = y[i];
+    for(uint32_t i = 0; i < nnz_rows___; i++)
+            values[rows2vals___[i]] = y[i];
     
     for(uint32_t i = 0; i < num_vertices; i++)
         value += values[i];
