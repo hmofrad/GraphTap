7ef0d5fe52c60826483038c8e9b3b53827552a6d
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-16 17:56:26 -0500

    Update base_csc.hpp

--- src/sim/base_csc.hpp
+++ src/sim/base_csc.hpp
@@ -1,5 +1,5 @@
 /*
- * base_csc.hpp: Base class for column compressed data structures
+ * Base_csc.hpp: Base_csc class for column compressed data structures
  * (c) Mohammad Mofrad, 2018
  * (e) m.hasanzadeh.mofrad@gmail.com 
  */
@@ -10,66 +10,49 @@
 #include <sys/mman.h>
 #include <cstring> 
 
-struct Base
-{
-    Base(uint64_t nnz_, uint32_t ncols_);
-    ~Base();
-    uint64_t nnz;
-    uint32_t ncols_plus_one;
-    uint64_t size;
-    
-    void *A;  // VAL
-    void *IA; // ROW_INDEX
-    void *JA; //COL_PTR
+struct Base_csc {
+    public:
+        Base_csc(uint64_t nnz_, uint32_t ncols_);
+        ~Base_csc();
+        uint64_t nnz;
+        uint32_t ncols_plus_one;
+        uint64_t size;    
+        void *A;  // VAL
+        void *IA; // ROW_INDEX
+        void *JA; //COL_PTR
 };
 
-Base::Base(uint64_t nnz_, uint32_t ncols_)
-{
+Base_csc::Base_csc(uint64_t nnz_, uint32_t ncols_) {
     nnz = nnz_;
     ncols_plus_one = ncols_ + 1;
-    if((A = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
-                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
-    {    
+    if((A = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
     memset(A, 0, nnz * sizeof(uint32_t));
-    
-    if((IA = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
-                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
-    {    
+    if((IA = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
     memset(IA, 0, nnz * sizeof(uint32_t));
-
-    if((JA = mmap(nullptr, ncols_plus_one * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS
-                                               | MAP_PRIVATE, -1, 0)) == (void*) -1)
-    {    
+    if((JA = mmap(nullptr, ncols_plus_one * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
     memset(JA, 0, ncols_plus_one * sizeof(uint32_t));
-    
     size = (nnz * sizeof(uint32_t)) + (nnz * sizeof(uint32_t)) + (ncols_plus_one * sizeof(uint32_t));
 }
 
-Base::~Base()
-{
-    if(munmap(A, nnz * sizeof(uint32_t)) == -1)
-    {
+Base_csc::~Base_csc() {
+    if(munmap(A, nnz * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
-    
-    if(munmap(IA, nnz * sizeof(uint32_t)) == -1)
-    {
+    if(munmap(IA, nnz * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
-    
-    if(munmap(JA, ncols_plus_one * sizeof(uint32_t)) == -1)
-    {
+    if(munmap(JA, ncols_plus_one * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
