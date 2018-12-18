926b7905857c425f7b26833072367a048e6635e6
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-17 22:24:49 -0500

    Update base_dcsc.hpp

--- src/sim/base_dcsc.hpp
+++ src/sim/base_dcsc.hpp
@@ -15,38 +15,43 @@ struct Base_dcsc {
         Base_dcsc(uint64_t nnz_, uint32_t nnzcols_);
         ~Base_dcsc();
         uint64_t nnz;
-        uint32_t nnzcols_plus_one;
+        uint32_t nnzcols;
         uint64_t size;
-        void *A;  // VAL
-        void *IA; // ROW_INDEX
-        void *JA; //COL_PTR
-        void *JC; //COL_INDEX
+        void *A;  // WEIGHT
+        void *IA; // ROW_IDX
+        void *JA; // COL_PTR
+        void *JC; // COL_IDX
 };
 
 Base_dcsc::Base_dcsc(uint64_t nnz_, uint32_t nnzcols_) {
     nnz = nnz_;
-    nnzcols_plus_one = nnzcols_ + 1;
+    nnzcols = nnzcols_;
+    
     if((A = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
     memset(A, 0, nnz * sizeof(uint32_t));
+    
     if((IA = mmap(nullptr, nnz * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
     memset(IA, 0, nnz * sizeof(uint32_t));
-    if((JA = mmap(nullptr, nnzcols_plus_one * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+    
+    if((JA = mmap(nullptr, (nnzcols + 1) * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
-    memset(JA, 0, nnzcols_plus_one * sizeof(uint32_t));
-    if((JC = mmap(nullptr, nnzcols_ * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+    memset(JA, 0, (nnzcols + 1) * sizeof(uint32_t));
+    
+    if((JC = mmap(nullptr, nnzcols * sizeof(uint32_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
         fprintf(stderr, "Error mapping memory\n");
         exit(1);
     }
-    memset(JC, 0, (nnzcols_plus_one - 1) * sizeof(uint32_t));
-    size = (nnz * sizeof(uint32_t)) + (nnz * sizeof(uint32_t)) + (nnzcols_plus_one * sizeof(uint32_t)) + ((nnzcols_plus_one - 1) * sizeof(uint32_t));
+    memset(JC, 0, nnzcols * sizeof(uint32_t));
+    
+    size = (nnz * sizeof(uint32_t)) + (nnz * sizeof(uint32_t)) + ((nnzcols + 1) * sizeof(uint32_t)) + (nnzcols * sizeof(uint32_t));
 }
 
 Base_dcsc::~Base_dcsc() {
@@ -54,15 +59,18 @@ Base_dcsc::~Base_dcsc() {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
+    
     if(munmap(IA, nnz * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
-    if(munmap(JA, nnzcols_plus_one * sizeof(uint32_t)) == -1) {
+    
+    if(munmap(JA, (nnzcols + 1) * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
-    if(munmap(JC, (nnzcols_plus_one - 1) * sizeof(uint32_t)) == -1) {
+    
+    if(munmap(JC, nnzcols * sizeof(uint32_t)) == -1) {
         fprintf(stderr, "Error unmapping memory\n");
         exit(1);
     }
