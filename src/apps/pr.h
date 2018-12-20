781ded2c40e07855cfb6284b4eb28f147b86e773
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-20 15:16:09 -0500

    Update src/apps/pr.h

--- src/apps/pr.h
+++ src/apps/pr.h
@@ -22,7 +22,7 @@ template<typename Weight, typename Integer_Type, typename Fractional_Type>
 class PR_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type, PR_State> {
     public: 
         using Vertex_Program<Weight, Integer_Type, Fractional_Type, PR_State>::Vertex_Program;
-        virtual bool initializer(Integer_Type vid, PR_State &state, const State &other) {
+        virtual bool initializer(Integer_Type vid, PR_State& state, const State& other) {
             state.degree = ((const Deg_State&) other).degree;
             state.rank = alpha; //  Not necessary
             return(true);
@@ -32,15 +32,15 @@ class PR_Program : public Vertex_Program<Weight, Integer_Type, Fractional_Type, 
             return( (state.degree) ? (state.rank / state.degree) : 0 );
         }
 
-        virtual void combiner(Fractional_Type &y1, const Fractional_Type &y2, const Fractional_Type &w) {
+        virtual void combiner(Fractional_Type& y1, const Fractional_Type& y2, const Fractional_Type& w) {
             y1 += (y2 * w);
         }
         
-        virtual void combiner(Fractional_Type &y1, const Fractional_Type &y2) {
+        virtual void combiner(Fractional_Type& y1, const Fractional_Type& y2) {
             y1 += y2;
         }
         
-        virtual bool applicator(PR_State &state, const Fractional_Type &y) {
+        virtual bool applicator(PR_State& state, const Fractional_Type& y) {
             Fractional_Type tmp = state.rank;
             state.rank = alpha + (1.0 - alpha) * y;
             return (fabs(state.rank - tmp) > tol);         
