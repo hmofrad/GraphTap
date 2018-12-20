b67c7e678456941f142950be7af8e30a7d321ce2
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-20 12:49:05 -0500

    Update pair.hpp

--- src/test/pair.hpp
+++ src/test/pair.hpp
@@ -9,22 +9,18 @@
 
 #include <algorithm>
 
-struct Pair
-{
+struct Pair {
     uint32_t row;
     uint32_t col;
 };
 
-struct ColSort
-{
-    bool operator()(const struct Pair &a, const struct Pair &b)
-    {
+struct ColSort {
+    bool operator()(const struct Pair &a, const struct Pair &b) {
         return((a.col == b.col) ? (a.row < b.row) : (a.col < b.col));
     }
 };
 
-void column_sort(std::vector<struct Pair> *pairs)
-{
+void column_sort(std::vector<struct Pair> *pairs) {
     ColSort f_col;
     std::sort(pairs->begin(), pairs->end(), f_col);
 }
