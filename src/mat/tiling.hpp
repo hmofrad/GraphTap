1bde7d3c716f95b9ce30de7fd7f63ada013e151d
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-21 14:59:59 -0500

    Update src/mat/tiling.hpp

--- src/mat/tiling.hpp
+++ src/mat/tiling.hpp
@@ -10,14 +10,12 @@
 #include <cassert> 
 #include <cmath>
  
-enum Tiling_type
-{
+enum Tiling_type {
   _2D_,
   _2DT_
 };
 
-class Tiling
-{    
+class Tiling {    
     template<typename Weight_, typename Integer_Type_, typename Fractional_Type_>
     friend class Matrix;
     template<typename Weight__, typename Integer_Type__, typename Fractional_Type__>
@@ -38,8 +36,7 @@ class Tiling
         void integer_factorize(uint32_t n, uint32_t& a, uint32_t& b);
 };
 
-Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_)
-{
+Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_) {
     nranks = nranks_;
     ntiles = ntiles_;
     nrowgrps = nrowgrps_;
@@ -47,16 +44,14 @@ Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t 
     rank_ntiles = ntiles_ / Env::nranks;
     tiling_type = tiling_type_;
     assert(rank_ntiles * nranks == ntiles);
-    if (tiling_type == Tiling_type::_2D_)
-    {
+    if (tiling_type == Tiling_type::_2D_) {
         integer_factorize(nranks, rowgrp_nranks, colgrp_nranks);
         assert(rowgrp_nranks * colgrp_nranks == nranks);
         rank_nrowgrps = nrowgrps / colgrp_nranks;
         rank_ncolgrps = ncolgrps / rowgrp_nranks;        
         assert(rank_nrowgrps * rank_ncolgrps == rank_ntiles);
     }
-    else if (tiling_type == Tiling_type::_2DT_)
-    {
+    else if (tiling_type == Tiling_type::_2DT_) {
         integer_factorize(nranks, rowgrp_nranks, colgrp_nranks);
         assert(rowgrp_nranks * colgrp_nranks == nranks);
         rank_nrowgrps = nrowgrps / colgrp_nranks;
@@ -67,12 +62,10 @@ Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t 
 
 Tiling::~Tiling() {};
 
-void Tiling::integer_factorize(uint32_t n, uint32_t& a, uint32_t& b)
-{
+void Tiling::integer_factorize(uint32_t n, uint32_t& a, uint32_t& b) {
   /* This approach is adapted from that of GraphPad. */
   a = b = sqrt(n);
-  while (a * b != n)
-  {
+  while (a * b != n) {
     b++;
     a = n / b;
   }
