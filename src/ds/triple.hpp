a6f4c617767815bfa802d3dc77976288e9f170ba
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-21 11:48:28 -0500

    Update src/ds/triple.hpp

--- src/ds/triple.hpp
+++ src/ds/triple.hpp
@@ -1,21 +1,19 @@
 /*
- * triple.hpp: Triple implementation
+ * triple.hpp: Triple data structure implementation
  * (c) Mohammad Mofrad, 2018
  * (e) m.hasanzadeh.mofrad@gmail.com 
  */
 
 #ifndef TRIPLE_HPP
 #define TRIPLE_HPP
- 
 template <typename Weight, typename Integer_Type = uint32_t>
-struct Triple
-{
+struct Triple {
     Integer_Type row;
     Integer_Type col;
     Weight weight;
     Triple(Integer_Type row = 0, Integer_Type col = 0, Weight weight = 0);
     ~Triple();
-    void set_weight(Weight &w);
+    void set_weight(Weight& w);
     Weight get_weight();
 };
 
@@ -27,22 +25,20 @@ template <typename Weight, typename Integer_Type>
 Triple<Weight, Integer_Type>::~Triple() {};
       
 template <typename Weight, typename Integer_Type>
-void Triple<Weight, Integer_Type>::set_weight(Weight &w)
-{
+void Triple<Weight, Integer_Type>::set_weight(Weight& w) {
     weight = w;
 }
 
 template <typename Weight, typename Integer_Type>
-Weight Triple<Weight, Integer_Type>::get_weight()
-{
+Weight Triple<Weight, Integer_Type>::get_weight() {
     return(weight);
 }
 
+/* Triples with empty weight */
 struct Empty {};
 
 template <typename Integer_Type>
-struct Triple <Empty, Integer_Type>
-{
+struct Triple <Empty, Integer_Type> {
     Integer_Type row;
     union {
         Integer_Type col;
@@ -60,7 +56,7 @@ struct Triple <Empty, Integer_Type>
 template <typename Weight, typename Integer_Type>
 struct RowSort
 {
-    bool operator()(const struct Triple<Weight, Integer_Type> &a, const struct Triple<Weight, Integer_Type> &b)
+    bool operator()(const struct Triple<Weight, Integer_Type>& a, const struct Triple<Weight, Integer_Type>& b)
     {
         #ifdef HAS_WEIGHT
         if(a.row < b.row)
@@ -82,7 +78,7 @@ struct RowSort
 template <typename Weight, typename Integer_Type>
 struct ColSort
 {
-    bool operator()(const struct Triple<Weight, Integer_Type> &a, const struct Triple<Weight, Integer_Type> &b)
+    bool operator()(const struct Triple<Weight, Integer_Type>& a, const struct Triple<Weight, Integer_Type>& b)
     {
         #ifdef HAS_WEIGHT
         if(a.col < b.col)
