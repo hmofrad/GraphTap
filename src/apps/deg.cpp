92a43a0bedbd7d181991ece1f6256953929713e7
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-21 23:19:36 -0500

    Update src/apps/deg.cpp

--- src/apps/deg.cpp
+++ src/apps/deg.cpp
@@ -12,8 +12,7 @@
 #include "deg.h"
 
 int main(int argc, char** argv) {
-    bool comm_split = true;
-    Env::init(comm_split);
+    Env::init();
     double time1 = Env::clock();
     if(argc != 3 and argc != 4) {
         if(Env::is_master)
@@ -30,19 +29,14 @@ int main(int argc, char** argv) {
     bool parallel_edges = true;
     Tiling_type TT = _2DT_;
     Compression_type CT = _CSC_;
-    Filtering_type FT = _SOME_;
-    Hashing_type HT = NONE;
-    bool parread = true;
     Graph<wp, ip, ip> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, FT, HT, parread);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT);
     bool stationary = true;
-    bool activity_filtering = false; // Has no effect (Always off)
-    bool tc_family = false;
     bool gather_depends_on_apply = false;
     bool apply_depends_on_iter  = false;
     Ordering_type OT = _ROW_;
     /* Degree execution */
-    Deg_Program<wp, ip, ip> V(G, stationary, activity_filtering, gather_depends_on_apply, apply_depends_on_iter, tc_family, OT);
+    Deg_Program<wp, ip, ip> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
     V.execute(num_iterations);
     V.checksum();
     V.display();
