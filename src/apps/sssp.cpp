ceb07db58752b31a1e977672f4e5c38403b66d4f
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-12-24 16:03:07 -0500

    Update src/apps/sssp.cpp

--- src/apps/sssp.cpp
+++ src/apps/sssp.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
     bool acyclic = false;
     bool parallel_edges = false;
     Tiling_type TT = _2DT_;
-    Compression_type CT = _CSC_; // Only CSC is supported
+    Compression_type CT = _DCSC_; // Only CSC is supported
     
     /* Single Source Shortest Path (SSSP) execution*/
     bool stationary = false;
