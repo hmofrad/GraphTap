54519cec2ab8ca719fdb02a243e8d216436fabd2
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-01-08 14:34:59 -0500

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     ip num_vertices = std::atoi(argv[2]);
     ip num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
     bool directed = true;
-    bool transpose = false;
+    bool transpose = true;
     bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = true;
@@ -37,24 +37,21 @@ int main(int argc, char** argv) {
     bool stationary = true;
     bool gather_depends_on_apply = false;
     bool apply_depends_on_iter  = false;
-    Ordering_type OT = _ROW_;
+    Ordering_type OT = _COL_;
     Deg_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
     V.execute(1);
     V.checksum();
-    G.free();
     Env::barrier();
-    
-    transpose = true;
-    Graph<wp, ip, fp> GR;    
-    GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT);
-    PR_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
+
+    OT = _ROW_;
+    PR_Program<wp, ip, fp> VR(G, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
     VR.initialize(V);
     V.free();
     VR.execute(num_iterations); // Vertex execution
     VR.checksum();
     VR.display();
     VR.free();
-    GR.free();
+    G.free();
     
     double time2 = Env::clock();
     Env::print_time("PageRank end-to-end", time2 - time1);
