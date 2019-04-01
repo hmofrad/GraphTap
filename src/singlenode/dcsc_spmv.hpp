e3c5bab21cc2afa669ea5d95322cb44a27a803d4
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-04-01 13:15:46 -0400

    Update dcsc_spmv.hpp

--- src/singlenode/dcsc_spmv.hpp
+++ src/singlenode/dcsc_spmv.hpp
@@ -237,9 +237,9 @@ void DCSC::update() {
 void DCSC::space() {
     total_size += dcsc->size;
     total_size += (((sizeof(uint32_t) * x.size())) + ((sizeof(uint32_t) + y.size())));
-    uint32_t nnzcols = dcsc->nnzcols;
-    total_size += (nnzcols * sizeof(uint32_t));
-    total_size += (nvertices * sizeof(uint32_t));
+    //uint32_t nnzcols = dcsc->nnzcols;
+    //total_size += (nnzcols * sizeof(uint32_t));
+    //total_size += (nvertices * sizeof(uint32_t));
 }
 
 double DCSC::checksum() {
